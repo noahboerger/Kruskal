c8a6e17001a071f7d8e1eb7a09dcef1d364cfd2a
noahboerger <35099943+noahboerger@users.noreply.github.com>
2018-06-16 13:56:05 +0200

    Ausgebengeht

--- src/starter.java
+++ src/starter.java
@@ -5,10 +5,9 @@ public class starter {
 	String filesep = System.getProperty("user.dir");
 	
 	public static void main(String[] args) throws IOException {
-		System.out.println("Tsa");
 		Graph testgraph = GraphIO.readGraph("C:\\Users\\noahb\\Desktop\\Testgraphen\\Graph.dat");
-		System.out.println("tds");
-		//TODO METHODE MOVEN ZU GRAPH
 		GraphIO.printGraph(testgraph);
+		
+		//Kruskal.kruskal(testgraph);
 	}
 }

9236053bee5a70f6d8f25807037d3db1f6871a92
noahboerger <35099943+noahboerger@users.noreply.github.com>
2018-06-16 13:57:46 +0200

    TEStölkldaf

--- src/starter.java
+++ src/starter.java
@@ -5,16 +5,12 @@ public class starter {
 	String filesep = System.getProperty("user.dir");
 	
 	public static void main(String[] args) throws IOException {
-<<<<<<< HEAD
+
 		Graph testgraph = GraphIO.readGraph("C:\\Users\\noahb\\Desktop\\Testgraphen\\Graph.dat");
-=======
-		System.out.println("Tsa");
-		Graph testgraph = GraphIO.readGraph("D:\\Hochschule Mosbach\\2. Semester\\Programmieren\\Kruskal Projekt\\Kruskal\\Graph.dat");
-		System.out.println("tds");
+		
 		//TODO METHODE MOVEN ZU GRAPH
->>>>>>> 95c5b12fd138b538980722f4a3bef7ef2ce6397c
 		GraphIO.printGraph(testgraph);
 		
-		//Kruskal.kruskal(testgraph);
+		Kruskal.kruskal(testgraph);
 	}
 }

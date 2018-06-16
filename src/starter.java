95c5b12fd138b538980722f4a3bef7ef2ce6397c
EricGendner <erigen@web.de>
2018-06-16 13:46:00 +0200

    :)

--- src/starter.java
+++ src/starter.java
@@ -6,7 +6,7 @@ public class starter {
 	
 	public static void main(String[] args) throws IOException {
 		System.out.println("Tsa");
-		Graph testgraph = GraphIO.readGraph("C:\\Users\\noahb\\Desktop\\Testgraphen\\Graph.dat");
+		Graph testgraph = GraphIO.readGraph("D:\\Hochschule Mosbach\\2. Semester\\Programmieren\\Kruskal Projekt\\Kruskal\\Graph.dat");
 		System.out.println("tds");
 		//TODO METHODE MOVEN ZU GRAPH
 		GraphIO.printGraph(testgraph);
